f72ce342082044cbdaa89215af07b86144ae4d12
TCashion <58757351+TCashion@users.noreply.github.com>
2020-03-02 15:18:54 -0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -31,7 +31,8 @@ $(document).ready(function () {
             $("#log-out-div").slideDown(250);
             email = user.email;
             uid = user.uid;
-            console.log(uid, email) ;
+            console.log(uid);
+            console.log(email);
             var welcomeMessage = `Welcome back, ${email}!`;
             $("#welcome-message").html(welcomeMessage);
             
