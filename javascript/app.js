d33bd09ab2e219744eaf5eda57ff66a461447f14
TCashion <58757351+TCashion@users.noreply.github.com>
2020-03-02 16:09:55 -0700

    run the addidnow function inside stoptimer

--- javascript/app.js
+++ javascript/app.js
@@ -219,6 +219,9 @@ $(document).ready(function () {
 
         // run stopwatch stop function
         stopClock();
+
+        // update current ID without refresh
+        addIDNow();
         
         // run update chart function
         updateChart();
@@ -291,13 +294,7 @@ $(document).ready(function () {
             end: stopTimeLegible
         });
 
-        // adds firebase id to table row so user can delete without refreshing (this happens automatically on refresh)
-        db.collection("timelog").where("date", "==", today).where("activity", "==", activityName).get().then(querySnapshot => {
-            querySnapshot.forEach(function (doc) {
-                var dataID = doc.id;
-                $("#data-id").attr("id", dataID);
-            });
-        });
+        
 
         // combines data if the activityName already exists
         for ( var i = 0; i < (chartLabels.length); i++) {
@@ -311,7 +308,15 @@ $(document).ready(function () {
         chartData.push(duration);           
     };
 
-    
+    function addIDNow () {
+        // adds firebase id to table row so user can delete without refreshing (this happens automatically on refresh)
+        db.collection("timelog").where("date", "==", today).where("activity", "==", activityName).get().then(querySnapshot => {
+            querySnapshot.forEach(function (doc) {
+                var dataID = doc.id;
+                $("#data-id").attr("id", dataID);
+            });
+        });
+    }
 
     function updateChart () {
         var chart = $("#chartView");
